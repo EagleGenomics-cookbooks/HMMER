6e52d0e432f89844c6c6d505614219fe2a1112c1
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-08-06 15:18:01 +0100

    Set as version 1.0.0 in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'all_rights'
 description 'Installs/Configures HMMER'
 long_description 'Installs/Configures HMMER'
-version '0.1.0'
+version '1.0.0'
 
 depends 'build-essential'
 depends 'magic_shell'
