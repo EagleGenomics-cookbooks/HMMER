e9f2782ac9dba0b2646dd0520458896acbf9a381
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-08-06 15:13:35 +0100

    Install working and tested

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,14 +10,39 @@ include_recipe 'build-essential'
 ##########################################################
 # here for use by serverspec
 
+magic_shell_environment 'HMMER_VERSION' do
+  value node['HMMER']['version']
+end
+
+magic_shell_environment 'HMMER_DIR' do
+  value node['HMMER']['dir']
+end
 
 ##########################################################
 # package install
 
+package ['zlib-devel'] do
+  action :install
+end
 
 ##########################################################
 # main recipe
 
+remote_file "#{Chef::Config[:file_cache_path]}/#{node['HMMER']['filename']}" do
+  source node['HMMER']['url']
+  action :create_if_missing
+end
+
+execute 'un-tar HMMER tar ball' do
+  command "tar zxvf #{Chef::Config[:file_cache_path]}/#{node['HMMER']['filename']} -C #{node['HMMER']['install_dir']}"
+  not_if { ::File.exist?("#{node['HMMER']['dir']}/README") }
+end
+
+# cannot use link with wild cards
+execute 'create symbolic links in PATH to executables' do
+  command "ln -s -f #{node['HMMER']['dir']}/binaries/* ."
+  cwd node['HMMER']['install_dir'] + '/bin'
+end
 
 ##########################################################
 ##########################################################
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,3 +3,67 @@ require_relative './spec_helper'
 
 # Required by serverspec
 set :backend, :exec
+
+describe file("#{ENV['HMMER_DIR']}") do
+  it { should be_directory }
+end
+
+%w(
+  alimask
+  esl-afetch
+  esl-alimanip
+  esl-alimap
+  esl-alimask
+  esl-alimerge
+  esl-alipid
+  esl-alistat
+  esl-cluster
+  esl-compalign
+  esl-compstruct
+  esl-construct
+  esl-histplot
+  esl-mask
+  esl-reformat
+  esl-selectn
+  esl-seqrange
+  esl-seqstat
+  esl-sfetch
+  esl-shuffle
+  esl-ssdraw
+  esl-stranslate
+  esl-weight
+  hmmalign
+  hmmbuild
+  hmmconvert
+  hmmemit
+  hmmerfm-exactmatch
+  hmmfetch
+  hmmlogo
+  hmmpgmd
+  hmmpress
+  hmmscan
+  hmmsearch
+  hmmsim
+  hmmstat
+  jackhmmer
+  makehmmerdb
+  nhmmer
+  nhmmscan
+  phmmer
+).each do |file_executable|
+  describe command("which #{file_executable}") do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command("#{file_executable} -h") do
+    its(:stdout) { should contain(ENV['HMMER_VERSION']) }
+  end
+end
+
+%w(
+  hmmc2
+).each do |file_executable|
+  describe command("which #{file_executable}") do
+    its(:exit_status) { should eq 0 }
+  end
+end
