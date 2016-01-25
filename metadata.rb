c18bba77cd5369b62cd67eeac9c17d038585b8fe
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-25 14:08:09 +0000

    License, maintainers, description etc

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'HMMER'
-maintainer 'The Authors'
+maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'all_rights'
+license 'apache2'
 description 'Installs/Configures HMMER'
-long_description 'Installs/Configures HMMER'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
 
 depends 'build-essential'
